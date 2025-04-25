51f947f7cf0282f94022f05f38adf76211a66347
shawnycx-dev <shawnycx.dev@gmail.com>
2025-04-24 22:34:26 -0500

    Fix form width

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -31,7 +31,7 @@ export default function Page() {
   const { name, email, message } = formState.data || {};
 
   return (
-    <div className="py-20 space-y-4">
+    <div className="py-20 space-y-4 max-w-full md:max-w-[80ch]">
       <div className="space-y-2">
         <h2 className="text-4xl text-balance font-bold">Let&apos;s get in touch!</h2>
         <p className="max-w-[80ch] font-montserrat font-light tracking-wide text-muted">
@@ -42,51 +42,49 @@ export default function Page() {
         </p>
       </div>
 
-      <div className="flex flex-row">
-        <form action={formAction} className="basis-1/2">
-          <div className="space-y-8">
-            <div className="flex flex-col gap-2">
-              <label htmlFor="name-field">Name</label>
-              <input
-                id="name-field"
-                type="text"
-                className={cn("border-2 border-dashed border-yellow-400/70 focus:border-yellow-400 focus:border-solid focus:outline-0 p-2 bg-gray-950", formState?.zodError?.name && "border-red-400/70 focus:border-red-400")}
-                placeholder="Name"
-                name="name"
-                defaultValue={name}
-              />
-              {formState?.zodError?.name && <p className="text-red-400/70">{formState?.zodError?.name}</p>}
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <label htmlFor="email-field">Email</label>
-              <input
-                id="email-field"
-                type="email"
-                className={cn("border-2 border-dashed border-yellow-400/70 focus:border-yellow-400 focus:border-solid focus:outline-0 p-2 bg-gray-950", formState?.zodError?.email && "border-red-400/70 focus:border-red-400")}
-                placeholder="Email"
-                name="email"
-                defaultValue={email}
-              />
-              {formState?.zodError?.email && <p className="text-red-400/70">{formState?.zodError?.email}</p>}
-            </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="message-field">Message</label>
-              <textarea
-                id="message-field"
-                className={cn("border-2 border-dashed border-yellow-400/70 focus:border-yellow-400 focus:border-solid focus:outline-0 p-2 bg-gray-950", formState?.zodError?.message && "border-red-400/70 focus:border-red-400")}
-                placeholder="Message"
-                name="message"
-                rows={5}
-                defaultValue={message}
-              />
-              {formState?.zodError?.message && <p className="text-red-400/70">{formState?.zodError?.message}</p>}
-            </div>
-            <SubmitButton />
-            <p aria-live="polite">{formState?.message}</p>
+      <form action={formAction}>
+        <div className="space-y-8">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="name-field">Name</label>
+            <input
+              id="name-field"
+              type="text"
+              className={cn("border-2 border-dashed border-yellow-400/70 focus:border-yellow-400 focus:border-solid focus:outline-0 p-2 bg-gray-950", formState?.zodError?.name && "border-red-400/70 focus:border-red-400")}
+              placeholder="Name"
+              name="name"
+              defaultValue={name}
+            />
+            {formState?.zodError?.name && <p className="text-red-400/70">{formState?.zodError?.name}</p>}
           </div>
-        </form>
-      </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email-field">Email</label>
+            <input
+              id="email-field"
+              type="email"
+              className={cn("border-2 border-dashed border-yellow-400/70 focus:border-yellow-400 focus:border-solid focus:outline-0 p-2 bg-gray-950", formState?.zodError?.email && "border-red-400/70 focus:border-red-400")}
+              placeholder="Email"
+              name="email"
+              defaultValue={email}
+            />
+            {formState?.zodError?.email && <p className="text-red-400/70">{formState?.zodError?.email}</p>}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="message-field">Message</label>
+            <textarea
+              id="message-field"
+              className={cn("border-2 border-dashed border-yellow-400/70 focus:border-yellow-400 focus:border-solid focus:outline-0 p-2 bg-gray-950", formState?.zodError?.message && "border-red-400/70 focus:border-red-400")}
+              placeholder="Message"
+              name="message"
+              rows={5}
+              defaultValue={message}
+            />
+            {formState?.zodError?.message && <p className="text-red-400/70">{formState?.zodError?.message}</p>}
+          </div>
+          <SubmitButton />
+          <p aria-live="polite">{formState?.message}</p>
+        </div>
+      </form>
     </div>
   );
 }
