cd33eb3f466ab1c1cedecef0b0c6e2f499e7c447
shawnycx-dev <shawnycx.dev@gmail.com>
2025-11-22 00:40:36 -0600

    Add payload

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig = {
   images: {
@@ -13,7 +14,7 @@ const nextConfig = {
   },
 } satisfies NextConfig;
 
-export default nextConfig;
+export default withPayload(nextConfig, { devBundleServerPackages: false });
 
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
