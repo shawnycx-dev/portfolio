fe74ce3e215018892029af7dee6cbd3563dacf8c
shawnycx-dev <shawnycx.dev@gmail.com>
2025-11-22 14:37:58 -0600

    Remove supabase migration

--- supabase/migrations/0000_fine_lethal_legion.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "contact_forms" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"email" text NOT NULL,
-	"message" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp NOT NULL
-);
