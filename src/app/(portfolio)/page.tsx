80cf6c221eecd7a57f74ab81bc690b9cf6d4d2d3
shawnycx-dev <shawnycx.dev@gmail.com>
2025-11-28 10:16:01 -0600

    Revalidate every hour

--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -1,5 +1,8 @@
 import { AboutSection, CompaniesSection, IntroSection, ProjectsSection } from "./_sections";
 
+// Revalidate every hour
+export const revalidate = 3600;
+
 export default function Home() {
   return (
     <>
