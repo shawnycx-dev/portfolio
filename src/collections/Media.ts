6a8f78644aaa803d20d15777e60b438a5c3ce0c7
shawnycx-dev <shawnycx.dev@gmail.com>
2025-11-28 11:19:04 -0600

    Convert uploads to webp

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,16 +1,24 @@
-import type { CollectionConfig } from 'payload'
+import type { CollectionConfig } from "payload";
+import sharp from "sharp";
 
 export const Media: CollectionConfig = {
-  slug: 'media',
+  slug: "media",
   access: {
     read: () => true,
   },
   fields: [
     {
-      name: 'alt',
-      type: 'text',
+      name: "alt",
+      type: "text",
       required: true,
     },
   ],
-  upload: true,
-}
+  upload: {
+    formatOptions: {
+      format: sharp.format.webp,
+      options: {
+        quality: 80,
+      },
+    },
+  },
+};
