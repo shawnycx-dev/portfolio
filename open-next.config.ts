ec40f5faa197258424ca541ac760ca1be6704d81
shawnycx-dev <shawnycx.dev@gmail.com>
2025-11-29 11:15:25 -0600

    Add configurations for revalidations

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,16 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
- 
+import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
+import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
+
+// OpenNext Caching Documentation:
+// https://opennext.js.org/cloudflare/caching
+
 export default defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
+  queue: doQueue,
+  // This is only required if you use On-demand revalidation
+  tagCache: d1NextTagCache,
+  // Disable this if you want to use PPR
+  enableCacheInterception: true,
 });
